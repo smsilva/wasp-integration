a4c06353dce89c2df5bcae68be9b98ae40d35924
Silvio Silva <smsilva@gmail.com>
2023-08-15 11:49:16 -0300

    feat: Initial config

---
+++ src/main/java/com/github/smsilva/wasp/integration/boundary/PeopleController.java
@@ -0,0 +1,18 @@
+package com.github.smsilva.wasp.integration.boundary;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("people")
+public class PeopleController {
+
+    @GetMapping
+    public ResponseEntity<?> home() {
+        return ResponseEntity
+                .ok("Hi!");
+    }
+
+}
